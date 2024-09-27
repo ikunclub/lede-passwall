f7554473082f2676e6593a226bd8a0071965114f
咪西咪西 <40599886+ikunclub@users.noreply.github.com>
2024-09-28 04:34:51 +0800

    Update chinadns-ng_update.sh

--- openwrt-chinadns-ng-luci/root/usr/share/chinadns-ng/chinadns-ng_update.sh
+++ openwrt-chinadns-ng-luci/root/usr/share/chinadns-ng/chinadns-ng_update.sh
@@ -8,7 +8,7 @@ wget https://ispip.clang.cn/all_cn_ipv6.txt -O /tmp/chnroute-v6.txt.tmp
 mv -f /tmp/chnroute-v6.txt.tmp /etc/chinadns-ng/chnroute6.txt
 wget https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt -O /tmp/gfwlist.txt.tmp
 mv -f /tmp/gfwlist.txt.tmp /etc/chinadns-ng/gfwlist.txt
-wget https://pexcn.me/daily/chinalist/chinalist.txt -O /tmp/chinalist.txt.tmp
+wget https://raw.githubusercontent.com/ikunclub/rules/refs/heads/main/chinadns/chinalist.txt -O /tmp/chinalist.txt.tmp
 mv -f /tmp/chinalist.txt.tmp /etc/chinadns-ng/chinalist.txt
 
 /etc/init.d/chinadns-ng restart
