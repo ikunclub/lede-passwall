738ef8999b29701fb7486b1d8f21ca9d65ab98f7
咪西咪西 <40599886+ikunclub@users.noreply.github.com>
2024-09-28 04:04:34 +0800

    Update chinadns-ng_update.sh

--- openwrt-chinadns-ng-luci/root/usr/share/chinadns-ng/chinadns-ng_update.sh
+++ openwrt-chinadns-ng-luci/root/usr/share/chinadns-ng/chinadns-ng_update.sh
@@ -2,11 +2,11 @@
 set -o pipefail
 
 [ -d /etc/chinadns-ng ] || mkdir /etc/chinadns-ng
-wget https://pexcn.me/daily/chnroute/chnroute.txt -O /tmp/chnroute.txt.tmp
+wget https://ispip.clang.cn/all_cn.txt -O /tmp/chnroute.txt.tmp
 mv -f /tmp/chnroute.txt.tmp /etc/chinadns-ng/chnroute.txt
-wget https://pexcn.me/daily/chnroute/chnroute-v6.txt -O /tmp/chnroute-v6.txt.tmp
+wget https://ispip.clang.cn/all_cn_ipv6.txt -O /tmp/chnroute-v6.txt.tmp
 mv -f /tmp/chnroute-v6.txt.tmp /etc/chinadns-ng/chnroute6.txt
-wget https://pexcn.me/daily/gfwlist/gfwlist.txt -O /tmp/gfwlist.txt.tmp
+wget https://fastly.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/gfw.txt -O /tmp/gfwlist.txt.tmp
 mv -f /tmp/gfwlist.txt.tmp /etc/chinadns-ng/gfwlist.txt
 wget https://pexcn.me/daily/chinalist/chinalist.txt -O /tmp/chinalist.txt.tmp
 mv -f /tmp/chinalist.txt.tmp /etc/chinadns-ng/chinalist.txt
